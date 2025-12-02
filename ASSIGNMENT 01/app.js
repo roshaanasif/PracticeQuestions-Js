b4eaa16965b5f19657650c817fb601b6aee7a7d0
Roshaan Asif <roshaanasif57@gmail.com>
2025-12-02 22:49:29 +0500

    practice JS
    alert  || string concatenation

---
+++ ASSIGNMENT 01/app.js
@@ -0,0 +1,130 @@
+//chapter 01
+
+// question 01
+
+// answer
+// alert("welcome to our website");
+
+// question 02
+
+// answer
+// alert("Error! please enter a valid password");
+
+// question 03
+
+// answer
+// alert("Welcome to JS land..\n happy coding!");
+
+// question 04
+
+// answer
+//alert("Welcome to JS land..");
+//alert("Happy coding! \nprevent this page from creating additional dialogs");
+
+
+// question 05
+
+// answer
+//console.log("hello.. i can run js through my web browser's console");
+
+
+// question 06
+
+// answer
+//alert("Welcome to my page");
+
+
+// chapter 02
+
+// question 01
+
+// answer
+// a="username";
+
+// question 02
+
+// answer
+// myName="Muhammad Roshaan"
+
+// question 03
+
+// answer
+// message="Hello world";
+// alert(message);
+
+// question 04
+
+// answer
+// name="Muhammad Roshaan";
+// alert(name);
+
+// age="16 years old";
+// alert(age);
+
+// message="certified mobile application developement ";
+// alert(message);
+
+
+
+// question 05
+
+// answer
+// a="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(a);
+
+// question 06
+
+// answer
+// email="name@123.com";
+// alert("my email address is "+ email);
+
+// question 07
+
+// answer
+// book="A smarter way to learn JavaScript";
+// alert("i am trying to learn from the book "+ book);
+
+// question 08
+
+// answer
+// document.write("Yah! i can write HTML content through JS");
+
+// question 09
+
+// answer
+// design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
+
+// question 10
+
+// answer
+// age=20;
+// alert("I am "+age+" years old");
+
+// question 11
+
+// answer
+// n=14;
+// alert("you visited this webpage "+n+" times");
+
+// question 12
+
+// answer
+// birthyear=2004;
+// document.write("my birth year is "+ birthyear);
+// document.write("<br>");
+// document.write("Data type of my declared variable is number");
+
+// question 13
+
+// answer
+// Name="John Doe";
+// product="T-shirts";
+// quantity=5;
+// store="XYZ Clothing store";
+// document.write(Name +" ordered "+quantity+"\n"+product+" on "+store);
+
+
+
+
